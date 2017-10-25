27174581616c4e7e4904f70a617855d14907398a
Gaulthier Gain <gaulthier.gain@student.ulg.ac.be>
2017-10-25 14:17:33 +0200

    Display network interfaces in GUI

--- app/src/main/java/com/montefiore/gaulthiergain/adhoclib/MainActivity.java
+++ app/src/main/java/com/montefiore/gaulthiergain/adhoclib/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.montefiore.gaulthiergain.adhoclib.services.InfosInterface;
@@ -30,12 +31,12 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Click", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), "Click", Toast.LENGTH_SHORT).show();
 
                 // Get infos Interface
                 InfosInterface infosInterface = new InfosInterface();
                 ArrayList<NetworkInterface> arrayNetworkInterfaces = infosInterface.getAllNetworkInterfaces();
-                displayInfosNetInterface(arrayNetworkInterfaces);
+                displayScreenInfosNetInterface(arrayNetworkInterfaces);
 
             }
         });
@@ -45,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
      * Display in console Infos Interface
      * @param arrayNetworkInterfaces array of network interfaces
      */
-    void displayInfosNetInterface(ArrayList<NetworkInterface> arrayNetworkInterfaces) {
+    void displayConsoleInfosNetInterface(ArrayList<NetworkInterface> arrayNetworkInterfaces) {
         for (int i = 0; i < arrayNetworkInterfaces.size(); i++) {
             Log.d("[AdHoc]", "Name: " + arrayNetworkInterfaces.get(i).getName());
 
@@ -73,5 +74,48 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+    }
+
+    /**
+     * Display in screen Infos Interface
+     * @param arrayNetworkInterfaces array of network interfaces
+     */
+    void displayScreenInfosNetInterface(ArrayList<NetworkInterface> arrayNetworkInterfaces) {
+
+        StringBuilder txt = new StringBuilder();
+        for (int i = 0; i < arrayNetworkInterfaces.size(); i++) {
+
+            txt.append("Name: ").append(arrayNetworkInterfaces.get(i).getName()).append("\n");
+
+            try {
+                if (arrayNetworkInterfaces.get(i).getHardwareAddress() != null) {
+                    txt.append("MAC: ").append(new String(arrayNetworkInterfaces.get(i).getHardwareAddress(), StandardCharsets.UTF_8)).append("\n");
+                } else {
+                    txt.append("MAC: /").append("\n");
+                }
+
+                txt.append("MTU: ").append(arrayNetworkInterfaces.get(i).getMTU()).append("\n");
+
+                Enumeration<InetAddress> en = arrayNetworkInterfaces.get(i).getInetAddresses();
+                while (en.hasMoreElements()) {
+                    InetAddress addr = en.nextElement();
+                    String s = addr.getHostAddress();
+                    int end = s.lastIndexOf("%");
+                    if (end > 0)
+                        txt.append("\tAddr: ").append(s.substring(0, end)).append("\n");
+                    else
+                        txt.append("\tAddr: ").append(s).append("\n");
+                }
+
+                txt.append("\n").append("\n");
+
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
+        }
+
+        TextView textView = (TextView) findViewById(R.id.textView1);
+        textView.setText(txt.toString());
+
     }
 }
