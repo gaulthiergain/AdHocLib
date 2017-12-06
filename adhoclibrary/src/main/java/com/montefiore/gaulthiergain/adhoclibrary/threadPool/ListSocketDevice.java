ffe4f3bb5bee21a5ebf5943e88923b3a396991df
Gaulthier Gain <gaulthier.gain@student.ulg.ac.be>
2017-12-06 15:27:57 +0100

    Add javadoc to listSocketDevice

--- adhoclibrary/src/main/java/com/montefiore/gaulthiergain/adhoclibrary/threadPool/ListSocketDevice.java
+++ adhoclibrary/src/main/java/com/montefiore/gaulthiergain/adhoclibrary/threadPool/ListSocketDevice.java
@@ -1,7 +1,5 @@
 package com.montefiore.gaulthiergain.adhoclibrary.threadPool;
 
-import android.util.Log;
-
 import com.montefiore.gaulthiergain.adhoclibrary.network.ISocket;
 import com.montefiore.gaulthiergain.adhoclibrary.network.NetworkObject;
 
@@ -9,50 +7,79 @@ import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * Created by gaulthiergain on 10/11/17.
+ * <p>This class allows to manage a list of clients used by the threadPool.</p>
+ * <p>The clients are represented by ISocket objects ({@link ISocket}).</p>
+ * <p>As this class is used by threads, all methods are synchronized.</p>
+ *
+ * @author Gaulthier Gain
+ * @version 1.0
  */
 public class ListSocketDevice {
-
-    private static final String TAG = "[AdHoc]";
 
     private ArrayList<ISocket> listISockets;
     private ConcurrentHashMap<String, NetworkObject> hashMapNetwork;
 
+    /**
+     * Constructor
+     */
     public ListSocketDevice() {
         listISockets = new ArrayList<>();
         hashMapNetwork = new ConcurrentHashMap<>();
     }
 
-    public synchronized ISocket getSocketDevice() throws InterruptedException {
-        Log.d(TAG, "Waiting Socket...");
+    /**
+     * Method allowing to get a ISocket from the listISockets list. This ISocket object represents
+     * a client which will be served when a thread is available.
+     *
+     * @return a ISocket object which represents a socket between the server and the client.
+     * @throws InterruptedException Thrown when a thread is waiting, sleeping, or occupied.
+     */
+    synchronized ISocket getSocketDevice() throws InterruptedException {
         while (listISockets.isEmpty()) {
             wait();
         }
         return listISockets.remove(0);
     }
 
+    /**
+     * Method allowing to add temporary a ISocket into the listISockets list.  This ISocket object
+     * represents a client which will be served until a thread is available.
+     *
+     * @param isocket a ISocket object which represents a socket between the server and the client.
+     */
+    synchronized void addSocketClient(ISocket isocket) {
 
-    public synchronized void addSocketClient(ISocket isocket) {
-
+        /* Add the ISocket object into hashMapNetwork to get a mapping between the remote address
+        and a networkObject (respectively a socket). */
         String key = isocket.getRemoteSocketAddress();
         if (!hashMapNetwork.containsKey(key)) {
             hashMapNetwork.put(key, new NetworkObject(isocket));
         }
 
         listISockets.add(isocket);
-        Log.d(TAG, "Add waiting Socket");
         notify();
     }
 
-    public ConcurrentHashMap<String, NetworkObject> getActiveConnexion() {
+    /**
+     * Method allowing to return a concurrent hashmap, to get a mapping between the remote address
+     * and a networkObject (respectively a socket).
+     *
+     * @return a ConcurrentHashMap<String, NetworkObject>.
+     */
+    synchronized ConcurrentHashMap<String, NetworkObject> getActiveConnexion() {
         return hashMapNetwork;
     }
 
-    public void removeActiveConnexion(ISocket isocket) {
+    /**
+     * Method allowing to remove an active connection (a socket) from the
+     * ConcurrentHashMap<String, NetworkObject>.
+     *
+     * @param isocket a ISocket object which represents a socket between the server and the client.
+     */
+    synchronized void removeActiveConnexion(ISocket isocket) {
         String key = isocket.getRemoteSocketAddress();
         if (hashMapNetwork.containsKey(key)) {
             hashMapNetwork.remove(isocket.getRemoteSocketAddress());
         }
     }
-
 }
